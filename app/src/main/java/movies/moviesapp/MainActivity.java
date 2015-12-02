a05ee4e93d0a4d68175f44bc6cdb434cc3e89bfe
24547744m <24547744m@github.com>
2015-12-02 20:27:50 +0100

    App funcionando con Settings activity creado, Sincronizacion al iniciarse y al hacer refresh desde el menu

--- app/src/main/java/movies/moviesapp/MainActivity.java
+++ app/src/main/java/movies/moviesapp/MainActivity.java
@@ -1,5 +1,6 @@
 package movies.moviesapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -27,13 +28,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+
+            startActivity(new Intent(this,SettingsActivity.class));
+
             return true;
         }
 
--- app/src/main/java/movies/moviesapp/MainActivityFragment.java
+++ app/src/main/java/movies/moviesapp/MainActivityFragment.java
@@ -15,16 +15,6 @@ import android.widget.ListView;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import movies.pojos.ApiData;
-import retrofit.Call;
-import retrofit.Callback;
-import retrofit.GsonConverterFactory;
-import retrofit.Response;
-import retrofit.Retrofit;
-import retrofit.http.GET;
-import retrofit.http.Path;
-import retrofit.http.Query;
-
 /**
  * A placeholder fragment containing a simple view.
  */
@@ -32,20 +22,16 @@ public class MainActivityFragment extends Fragment {
 
     private ArrayList<String> items;
     private ArrayAdapter<String> adapter;
-    private final String API_KEY = "392610fb7cf880a09d912568ce434ef2";
-    private final String BASE_URL = "http://api.themoviedb.org/3/";
 
 
     public MainActivityFragment() {
     }
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);//seteando el menu
     }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -79,17 +65,18 @@ public class MainActivityFragment extends Fragment {
         return rootView;
 
     }
-
+    @Override
+    public void onStart() {
+        super.onStart();
+        refresh();
+    }
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.menu_main_fragment, menu);//agregando items al menu
     }
-
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
         int id = item.getItemId();
 
         if (id == R.id.action_refresh){
@@ -97,63 +84,21 @@ public class MainActivityFragment extends Fragment {
         }
 
         return super.onOptionsItemSelected(item);
-
     }
-
 
     /**
      * Metodo para refrescar los valores del listView
      * */
     private void refresh() {
-        Retrofit retrofit = new Retrofit.Builder()
-                                        .baseUrl(BASE_URL)
-                                        .addConverterFactory(GsonConverterFactory.create())
-                                        .build();
+        Log.d("refresshh","...");
 
-        MoviesInterface serviceInterface = retrofit.create(MoviesInterface.class);//Creación del servicio
-//        Log.d("holaaaaaa", "aahahahaha");
-
-//        Call<ApiData> call = serviceInterface.getPopularMovies(API_KEY);
-        Call<ApiData> call = serviceInterface.getRatedMovies(API_KEY);
-        call.enqueue(new Callback<ApiData>() {
-            @Override
-            public void onResponse(Response<ApiData> response, Retrofit retrofit) {
-                if (response.isSuccess()){
-                    Log.d("Petición: ","OKKKKKKKKK");
-//                    ApiData apiData = response.body();
-                    Log.e("respuesta ", response.message());
-                    Log.e("Datos ", String.valueOf(response.body().getTotalResults()));
-
-
-                }else{
-                    Log.e("Error", response.errorBody().toString());
-
-                }
-//                    Log.d("Petición: ","NOOOOO");
-
-            }
-
-            @Override
-            public void onFailure(Throwable t) {
-
-            }
-        });
+        MovieDBAPIClientRetrofit apiClient = new MovieDBAPIClientRetrofit();
+        apiClient.getPopularMovies(adapter);
 
     }
 
 
-    /**
-     * Interfaz para hacer las llamadas desde Retrofit
-     * */
-    public interface MoviesInterface{
-        @GET("movie/popular")
-        Call<ApiData> getPopularMovies(@Query("api_key") String apiKey);
 
-        @GET("movie/top_rated")
-        Call<ApiData> getRatedMovies(@Query("api_key") String apiKey);
-
-
-    }
 
 
 }
